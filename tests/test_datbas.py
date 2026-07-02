76a32f94fab7f1764f34b55ccd09d5ccffdf1988
Vashokshok <vashokshok@mail.ru>
2026-07-02 18:43:13 +0300

    add posledny

--- tests/test_datbas.py
+++ tests/test_datbas.py
@@ -1,13 +1,19 @@
-from table.models import Category, Base
+from table.models import Category, Base, Note
 from table.database import session, engine
 
 Base.metadata.create_all(engine)
 
-def test_add_dealer():
+def test_add_cotegor():
     deaker = Category(name='Khava')
     session.add(deaker)
     session.commit()
 
     saved = session.query(Category).filter_by(name='Khava').first()
     assert saved is not None
+def test_add_note():
+    note = Note(title='NKnlk', content='na')
+    session.add(note)
+    session.commit()
 
+    saved = session.query(Category).filter_by(title='NKnlk').first()
+    assert saved is not None
